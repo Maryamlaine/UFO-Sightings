89c6a58666844f025fe189c2482a339e60eae9f2
homelaptop <m.tab59@gmail.com>
2020-05-08 12:27:48 -0500

    updating app_2

--- static/js/app_2.js
+++ static/js/app_2.js
@@ -43,8 +43,6 @@ function runEnter() {
     var queryid = query.attr("id");
     console.log(query);
     console.log(queryid);
-    // console.log(query.attr("id"))
-    //console.log(query.property("value"))
     console.log(queryvalue);
     inputValues[queryid] = queryvalue;
     console.log(inputValues);
@@ -72,46 +70,6 @@ function newtable(){
     });
     };
 
-  // Select the input element and get the raw HTML node
-//   var inputDateElement = d3.select("#datetime");
-//   var inputCityElement = d3.select("#city");
-//   var inputStateElement = d3.select("#state");
-//   var inputCountryElement = d3.select("#country");
-//   var inputShapeElement = d3.select("#shape");
-
-//   // Get the value property of the input element
- //   var inputDateValue = inputDateElement.property("value"); 
-//   var inputCityValue = inputCityElement.property("value");
-//   var inputStateValue = inputStateElement.property("value");
-//   var inputCountryValue = inputCountryElement.property("value");
-//   var inputShapeValue = inputShapeElement.property("value");
-
-
-  // Print the value to the console
-//   console.log(inputDateValue);
-//   console.log(inputCityValue);
-//   console.log(inputStateValue);
-//   console.log(inputCountryValue);
-//   console.log(inputShapeValue);
-  // console.log(tableData);
-
-// var filteredCity = tableData.filter(ufo => ufo.city === inputCityValue);
-// var filteredState = tableData.filter(ufo => ufo.state === inputStateValue);
-// var filteredCountry = tableData.filter(ufo => ufo.country === inputCountryValue);
-// var filteredShape = tableData.filter(ufo => ufo.shape === inputShapeValue);
-
-// console.log(filteredDate);
-// console.log(filteredCity);
-// console.log(filteredState);
-// console.log(filteredCountry);
-// console.log(filteredShape);
-
-//Clear fields
-// inputDateValue = '';
-// inputCityValue = '';
-// inputStateValue = '';
-// inputCountryValue = '';
-// inputShapeValue = '';
 
 
 
